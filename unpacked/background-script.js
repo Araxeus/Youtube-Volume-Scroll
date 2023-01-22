7c34ed876beaca3c70400578912662e47192efd2
Araxeus <oaeben@gmail.com>
2023-01-22 02:09:03 +0200

    [Firefox only] prompt for permissions on install (#23)

---
+++ unpacked/background-script.js
@@ -0,0 +1,17 @@
+// This file is only included in the firefox version of the extension
+// Purpose: Prompt the user to grant the extension permission to access youtube.com and music.youtube.com
+// This is essential on firefox since the browser doesn't ask for permission by itself
+
+const browserApi = globalThis.browser ?? globalThis.chrome ?? null;
+
+const permissions = {
+    origins: ['https://www.youtube.com/*', 'https://music.youtube.com/*']
+};
+browserApi.permissions.contains(permissions, result => {
+    if (!result) {
+        browserApi.browserAction?.openPopup?.()
+            ?? browserApi.action?.openPopup?.()
+            ??
+            browserApi.tabs.create({ url: browserApi.runtime.getURL('popup/popup.html') });
+    }
+});
