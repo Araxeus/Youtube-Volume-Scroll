a0f65c09a87a4f4c0370c3bb4ded3b481c8df87d
Araxeus <78568641+Araxeus@users.noreply.github.com>
2023-01-22 02:04:53 +0200

    add note on background-script

--- unpacked/background-script.js
+++ unpacked/background-script.js
@@ -1,3 +1,7 @@
+// This file is only included in the firefox version of the extension
+// Purpose: Prompt the user to grant the extension permission to access youtube.com and music.youtube.com
+// This is essential on firefox since the browser doesn't ask for permission by itself
+
 const browserApi = globalThis.browser ?? globalThis.chrome ?? null;
 
 const permissions = {
